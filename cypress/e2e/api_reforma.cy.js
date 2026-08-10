3e1f542e095469627075b514a6a32024f7c4a96e
flpeac <flpeac@gmail.com>
2026-08-10 11:34:21 -0300

    Criação dos cenários de retorno da API (200, 400)

--- cypress/e2e/api_reforma.cy.js
+++ cypress/e2e/api_reforma.cy.js
@@ -1,11 +1,67 @@
-// cypress/e2e/api_reforma.cy.js
-
 describe('Testes de API - Calculadora Reforma Tributária', () => {
+
+    const apiURL = 'http://localhost:3000/api/calcular-tributos'
+
+    it('Status 200 - Sucesso', () => {
+        cy.request({
+            method: 'POST',
+            url: apiURL,
+            body: {
+                cenario: 'padrao',
+                pVenda: 100
+            }
+        }).then((response) => {
+            expect(response.status).to.eq(200)
+        })
+    })
+
+    it('Status 400 - Bad Request', () => {
+        cy.request({
+            method: 'POST',
+            url: apiURL,
+            failOnStatusCode: false,
+            body: {
+                pVenda: 100
+            }
+        }).then((response) => {
+            expect(response.status).to.eq(400)
+        })
+    })
+
+    it('Status 400 - Cenário desconhecido', () => {
+        cy.request({
+            method: 'POST',
+            url: apiURL,
+            failOnStatusCode: false,
+            body: {
+                pVenda: 100,
+                cenario: 'cenario-inválido'
+            }
+        }).then((response) => {
+            expect(response.status).to.eq(400)
+        })
+    })
+
+    it('Status 500 - Internal Server Error', () => {
+        cy.request({
+            method: 'POST',
+            url: 'http://localhost:3000/api/quebra',
+            failOnStatusCode: false,
+            body: {
+                cenario: 'padrao',
+                pVenda: 100
+            }
+        }).then((response) => {
+            expect(response.status).to.eq(500)
+        })
+
+
+    })
 
     it('Deve calcular corretamente o cenário padrão com Preço de Venda Cheio(BASECBSIBS_1)', () => {
         cy.request({
             method: 'POST',
-            url: 'http://localhost:3000/api/calcular-tributos',
+            url: apiURL,
             body: {
                 cenario: 'padrao',
                 pVenda: 100,
@@ -24,7 +80,7 @@ describe('Testes de API - Calculadora Reforma Tributária', () => {
     it('Deve calcular corretamente o cenário padrão com Preço de Venda Quebrado (BASECBSIBS_1', () => {
         cy.request({
             method: 'POST',
-            url: 'http://localhost:3000/api/calcular-tributos',
+            url: apiURL,
             body: {
                 cenario: 'padrao',
                 pVenda: 50.30,
@@ -42,11 +98,11 @@ describe('Testes de API - Calculadora Reforma Tributária', () => {
 
     it('Deve apresentar mensagem informando erro para preço de venda negativo', () => {
         cy.request({
-            method: 'POST', 
-            url: 'http://localhost:3000/api/calcular-tributos', 
+            method: 'POST',
+            url: apiURL,
             failOnStatusCode: false,
             body: {
-                cenario: 'padrao', 
+                cenario: 'padrao',
                 pVenda: -12.50,
                 cbs: 0.9,
                 ibs: 0.1
@@ -61,7 +117,7 @@ describe('Testes de API - Calculadora Reforma Tributária', () => {
     it('Deve calcular corretamente o cenário com acréscimos e tributos (BASECBSIBS_2)', () => {
         cy.request({
             method: 'POST',
-            url: 'http://localhost:3000/api/calcular-tributos',
+            url: apiURL,
             body: {
                 cenario: 'padraoBase_2',
                 pVenda: 100,
@@ -78,5 +134,24 @@ describe('Testes de API - Calculadora Reforma Tributária', () => {
         })
     })
 
+
+    it('Deve retornar erro se o cenário não for enviado', () => {
+        cy.request({
+            method: 'POST',
+            url: apiURL,
+            failOnStatusCode: false,
+            body: {
+                pVenda: 100,
+                cbs: 0.9,
+                ibs: 0.1,
+                vlAcres: 30,
+                vlTribut: 18
+            }
+        }).then((response) => {
+            expect(response.status).to.eq(400)
+            expect(response.body.erro).to.eq("O campo 'cenario' é obrigatório.")
+
+        })
+    })
 
 })
